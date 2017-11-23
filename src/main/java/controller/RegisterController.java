58b673a97ffeafa5348030ac3488bceec8bec200
Xiaowawa <wabanxian@gmail.com>
2017-11-23 16:02:39 +0800

    hf hf

--- src/main/java/controller/RegisterController.java
+++ src/main/java/controller/RegisterController.java
@@ -8,6 +8,7 @@ import domain.Usr;
 import org.springframework.beans.factory.annotation.Autowired;
 
 
+import org.springframework.boot.ExitCodeEvent;
 import org.springframework.stereotype.Controller;
 
 
@@ -17,6 +18,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import service.Usrservice;
+
+import java.util.List;
 
 
 @Controller
